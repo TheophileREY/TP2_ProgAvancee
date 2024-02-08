4ca240525dff70de03bd7859f4be7c815ebe8e48
Théophile REY <83590565+TheophileREY@users.noreply.github.com>
2024-02-08 13:44:44 +0000

    Update main.cpp

--- Question1/main.cpp
+++ Question1/main.cpp
@@ -1,8 +1,10 @@
 #include "forme.h"
 
 int main() {
-    Forme forme(70, 150, 100); // Exemple 
-    int result = forme.compute();
-    Forme::displayResult(result);
+    Forme forme;
+    forme.setF0(70); // Exemple
+    forme.setF1(150); // Exemple
+    forme.setF2(100); // Exemple
+    cout << forme << endl;
     return 0;
 }
