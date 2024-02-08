2bd14b2fe9c256ebeacbce6620da0a67d50d5bf5
Théophile REY <83590565+TheophileREY@users.noreply.github.com>
2024-02-08 13:47:08 +0000

    Update forme.cpp

--- Question1/forme.cpp
+++ Question1/forme.cpp
@@ -1,21 +1,30 @@
 #include "forme.h"
 
+Forme::Forme() : f0(0), f1(0), f2(0) {}
 Forme::Forme(int f0, int f1, int f2) : f0(f0), f1(f1), f2(f2) {}
-
-int Forme::compute() {
+void Forme::setF0(int f0) { this->f0 = f0; }
+void Forme::setF1(int f1) { this->f1 = f1; }
+void Forme::setF2(int f2) { this->f2 = f2; }
+int Forme::compute() const {
     return 0.1 * (f0 + f1 + f2 - 200);
 }
 
-void Forme::displayResult(int result) {
+string Forme::ObtenirResult() const {
+    int result = compute();
     if (result <= 0) {
-        cout << "Condition excellente." << endl;
+        return "Condition excellente.";
     } else if (result > 0 && result < 5) {
-        cout << "Condition très bonne." << endl;
+        return "Condition très bonne.";
     } else if (result >= 5 && result < 10) {
-        cout << "Condition bonne." << endl;
+        return "Condition bonne.";
     } else if (result >= 10 && result < 15) {
-        cout << "Condition moyenne." << endl;
+        return "Condition moyenne.";
     } else {
-        cout << "Condition faible." << endl;
+        return "Condition faible.";
     }
+}
+
+ostream& operator<<(ostream& os, const Forme& forme) {
+    os << forme.ObtenirResult();
+    return os;
 }
