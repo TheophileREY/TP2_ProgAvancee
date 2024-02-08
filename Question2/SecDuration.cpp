dd339140eae83e06fe458ce0fd92d317c8590198
Théophile REY <83590565+TheophileREY@users.noreply.github.com>
2024-02-08 14:19:26 +0000

    Update SecDuration.cpp

--- Question2/SecDuration.cpp
+++ Question2/SecDuration.cpp
@@ -1,17 +1,28 @@
+#include "FullDuration.cpp"
+
 class SecDuration {
 private:
     int seconds;
 
 public:
-    // Constructeur par défaut, pour initialiser seconds à 0
     SecDuration() : seconds(0) {}
-    // Constructeur avec paramètre, pour initialiser seconds
     SecDuration(int s) : seconds(s) {}
-    // Getters et Setters pour seconds
     int getSeconds() const {
         return seconds;
     }
     void setSeconds(int s) {
         seconds = s;
     }
+    FullDuration toFullDuration() const;
 };
+
+FullDuration SecDuration::toFullDuration() const {
+    int totalSeconds = seconds;
+    int d = totalSeconds / (24 * 3600);
+    totalSeconds %= (24 * 3600);
+    int h = totalSeconds / 3600;
+    totalSeconds %= 3600;
+    int m = totalSeconds / 60;
+    int s = totalSeconds % 60;
+    return FullDuration(d, h, m, s);
+}
