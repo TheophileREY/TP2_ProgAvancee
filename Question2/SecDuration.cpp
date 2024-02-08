e38068dec7e92d0070d6f99acc9eb411547860e4
Théophile REY <83590565+TheophileREY@users.noreply.github.com>
2024-02-08 15:20:25 +0000

    Update SecDuration.cpp

--- Question2/SecDuration.cpp
+++ Question2/SecDuration.cpp
@@ -1,4 +1,4 @@
-#include "FullDuration.cpp" 
+#include "FullDuration.cpp"
 
 class SecDuration {
 private:
@@ -7,7 +7,7 @@ private:
 public:
     SecDuration() : seconds(0) {}
     SecDuration(int s) : seconds(s) {}
-
+    
     // Constructeur
     SecDuration(int d, int h, int m, int s) {
         seconds = s + m * 60 + h * 3600 + d * 24 * 3600;
@@ -18,16 +18,31 @@ public:
     void setSeconds(int s) {
         seconds = s;
     }
-    FullDuration toFullDuration() const;
+    void increment(int sec = 1) {
+        seconds += sec;
+    }
+    void incrementMinutes(int min = 1) {
+        increment(min * 60);
+    }
+    void incrementHours(int hour = 1) {
+        increment(hour * 3600);
+    }
+    void incrementDays(int day = 1) {
+        increment(day * 86400);
+    }
+    FullDuration toFullDuration() const {
+        int totalSeconds = seconds;
+        int d = totalSeconds / (24 * 3600);
+        totalSeconds %= (24 * 3600);
+        int h = totalSeconds / 3600;
+        totalSeconds %= 3600;
+        int m = totalSeconds / 60;
+        int s = totalSeconds % 60;
+        return FullDuration(d, h, m, s);
+    }
 };
 
-FullDuration SecDuration::toFullDuration() const {
-    int totalSeconds = seconds;
-    int d = totalSeconds / (24 * 3600);
-    totalSeconds %= (24 * 3600);
-    int h = totalSeconds / 3600;
-    totalSeconds %= 3600;
-    int m = totalSeconds / 60;
-    int s = totalSeconds % 60;
-    return FullDuration(d, h, m, s);
+SecDuration FullDuration::toSecDuration() const {
+    int totalSeconds = seconds + minutes * 60 + hours * 3600 + days * 24 * 3600;
+    return SecDuration(totalSeconds);
 }
