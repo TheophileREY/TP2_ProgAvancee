e96c91eed6390e409842b233d80b7fefd14d6e20
Théophile REY <83590565+TheophileREY@users.noreply.github.com>
2024-03-20 15:38:11 +0100

    Create Main.java

---
+++ Question1_Java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        FormePhysique forme = new FormePhysique();
+        forme.setF0(70);
+        forme.setF1(120);
+        forme.setF2(80);
+
+        System.out.println(forme);
+    }
+}
